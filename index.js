8aa15d61b3116ad4572a3abb10e8b23afeb3222d
haylove <hfayelovelady@gmail.com>
2022-11-16 19:11:27 -0600

    guestions for command line

---
+++ index.js
@@ -0,0 +1,10 @@
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const employee = require('./employee.js');
+const { manager } = require('./manager.js');
+const { engineer } = require('./engineer.js');
+const { intern } = require('./intern.js');
+
+
+
